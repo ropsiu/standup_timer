5a672e2a7da5efcca3477d52cba429390dd1d59c
ropsiU <r.awdziej@gmail.com>
2015-10-05 22:44:09 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+
+gulp.task('bower', function() {
+    return gulp.src(mainBowerFiles(), {
+        base: 'bower_components'
+      })
+      .pipe(gulp.dest('public/lib'));
+});
+ 
+gulp.task('default', ['bower']);
